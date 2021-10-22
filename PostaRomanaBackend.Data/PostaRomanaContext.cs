aaf95969c64472581061314e96654372783a9b6a
AndreiTuguiu <91051950+AndreiTuguiu@users.noreply.github.com>
2021-10-22 09:42:33 +0300

    changed server details to the actual database in dbcontext

--- PostaRomanaBackend.Data/PostaRomanaContext.cs
+++ PostaRomanaBackend.Data/PostaRomanaContext.cs
@@ -27,7 +27,7 @@ namespace PostaRomanaBackend.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.;Database=Mock;Trusted_Connection=False;User Id=sa;Password=Fistikiu21!;MultipleActiveResultSets=true");
+                optionsBuilder.UseSqlServer("Server=TS1678\\SQLEXPRESS,1091;Database=PostaRomana;Trusted_Connection=False;User Id=Internship;Password=123;MultipleActiveResultSets=true");
             }
         }
 
