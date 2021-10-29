8392d38ace3b31a1263f8a0ffbaac643a4908d3c
AndreiTuguiu <91051950+AndreiTuguiu@users.noreply.github.com>
2021-10-29 16:05:09 +0300

    turned login from post to get

--- PostaRomanaBackend.WebApi/Controllers/AccountController.cs
+++ PostaRomanaBackend.WebApi/Controllers/AccountController.cs
@@ -84,7 +84,7 @@ namespace PostaRomanaBackend.WebApi.Controllers
             return "OK";
         }
 
-        [HttpPost]
+        [HttpGet]
         [Route("LogTheUser")]
         public async Task<DateTime> LogInUser([FromQuery] LogInOperations.Query query, CancellationToken cancellationToken)
         {
